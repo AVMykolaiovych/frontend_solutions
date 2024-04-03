faa45dcca979844636ef6384c98a91abd3d264cb
AVMykolaiovych <webdev.pitt@gmail.com>
2024-04-02 23:34:34 -0400

    Add switch theme to code

--- app/js/app.js
+++ app/js/app.js
@@ -10,4 +10,36 @@ document.addEventListener("DOMContentLoaded", () => {
             body.classList.toggle("collapsed");
         });
     }
+
+    // Footer
+    const year = document.querySelector("#current-year");
+    year.innerHTML = new Date().getFullYear();
+
+    // Switch
+    const toggleBtn = document.getElementById("toggle-btn");
+    const theme = document.querySelector("body");
+    let darkMode = localStorage.getItem("dark-mode");
+
+    const enableDarkMode = () => {
+        theme.classList.add("dark-mode-theme");
+        toggleBtn.classList.remove("dark-mode-toggle");
+        localStorage.setItem("dark-mode", "enabled");
+    };
+
+    const disableDarkMode = () => {
+        theme.classList.remove("dark-mode-theme");
+        toggleBtn.classList.add("dark-mode-toggle");
+        localStorage.setItem("dark-mode", "disabled");
+    };
+
+    if (darkMode === "enabled") {
+        enableDarkMode();
+    }
+
+    toggleBtn.addEventListener("click", (e) => {
+        darkMode = localStorage.getItem("dark-mode");
+        if (darkMode === "disabled") {
+            enableDarkMode();
+        } else disableDarkMode();
+    });
 });
